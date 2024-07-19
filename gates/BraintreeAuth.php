29c5b800d0ee6c9a6c95cb49258d21aceed5482a
Omthakkar15 <120113353+Omthakkar15@users.noreply.github.com>
2024-07-19 19:05:05 +0530

    Delete gates directory

--- gates/BraintreeAuth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-  if (preg_match('/^(\/brr|\.brr|!brr)/', $text)) {
-    $userid = $update['message']['from']['id'];
-
-      $sent_message_id = send_reply($chatId, $message_id, $keyboard, "<b>THIS GATE ON MAINTENANCE ❌</b>");
-
-
-}
